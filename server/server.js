16a917083b25fbe9885ce8cdc730445127531a3f
kikiran <kiran.m79955@gmail.com>
2024-11-05 10:19:16 +0530

    added build script

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import path from "path";
 import products from "./routes/products.js";
 dotenv.config();
 
@@ -12,6 +13,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/products", products);
 
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 app.listen(port, () => {
   connectDB(), console.log(`The application is running on Port ${port}`);
 });
